0ed9e50c2060287e57365052279efee36fb02492
crazy0codes <madhanpanja@gmail.com>
2025-06-18 12:22:13 +0530

    Project init

---
+++ ecap/src/main/java/com/student/ecap/EcapApplication.java
@@ -0,0 +1,13 @@
+package com.student.ecap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcapApplication.class, args);
+	}
+
+}
