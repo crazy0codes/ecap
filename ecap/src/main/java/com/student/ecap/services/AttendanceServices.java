ac2eddb4565350df8f0cb2b55bf36050cbda8ef0
crazy0codes <madhanpanja@gmail.com>
2025-06-19 10:39:19 +0530

    Attendance completed

--- ecap/src/main/java/com/student/ecap/services/AttendanceServices.java
+++ ecap/src/main/java/com/student/ecap/services/AttendanceServices.java
@@ -4,20 +4,21 @@ import com.student.ecap.entities.AttendanceEntity;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.student.ecap.respository.AttendanceRepository;
 
 @Service
 public class AttendanceServices {
 
     @Autowired
-    AttendanceRespository attendanceRespository;
+    AttendanceRepository attendanceRepository;
 
     public AttendanceEntity getById(String studentId){
         ObjectId id = new ObjectId(studentId);
-        return attendanceRespository.getByStudentId(id);
+        return attendanceRepository.getStudentById(studentId);
     }
 
     public AttendanceEntity update(AttendanceEntity attendance){
-        return attendanceRespository.save(attendance);
+        return attendanceRepository.save(attendance);
     }
 
 }
